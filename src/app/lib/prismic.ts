dafcfd9124c6ffee37239c54ac744014750c3a14
Alexis Jobart <alexisjobart@macbook-pro.home>
2024-10-13 21:09:17 +0200

    PORTFOLIO-01 : delete comment

--- src/app/lib/prismic.ts
+++ src/app/lib/prismic.ts
@@ -4,5 +4,4 @@ export const repositoryName = import.meta.env.VITE_PRISMIC_REPOSITORY_NAME
 
 export const client = prismic.createClient(repositoryName, {
   accessToken: 'your-access-token',
-  // Add your repository name here
 })
