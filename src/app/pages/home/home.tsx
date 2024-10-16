59fc6439ae36e83f02ef4ccd56dc24688baf37d6
Alexis Jobart <jobart.pro@gmail.com>
2024-10-16 21:16:38 +0200

    PORTFOLIO-002 : improve herobanner and wip about section

--- src/app/pages/home/home.tsx
+++ src/app/pages/home/home.tsx
@@ -37,10 +37,10 @@ export default function Home() {
     return (
         <div className={`relative w-full h-fit flex flex-col items-start justify-start ${isDarkMode ? 'dark bg-home-dark text-primary-dark' : 'bg-home-light text-primary-light'}`}>
             <Navbar />
-            <div className="mt-[64px] w-full flex flex-col px-[246px] box-border text-primary-light bg-home-light dark:bg-home-dark dark:text-primary-dark overflow-scroll">
-                {/* CONTENT CONTAINER */}
+            <div className="grid-background mt-[64px] w-full flex flex-col px-[246px] box-border text-primary-light bg-home-light dark:bg-home-dark dark:text-primary-dark overflow-scroll">
                 <div className="w-full h-full">
-                    <div className="h-[520px] w-full flex items-center justify-between">
+                    {/* SECTION HEROBANNER */}
+                    <div className="h-[580px] w-full flex items-center justify-between">
                         <div className="h-full w-full flex flex-col items-start justify-center">
                             <div className="flex items-end gap-2">
                                 <h1 className="text-4xl font-bold">Hi,</h1>
@@ -57,35 +57,34 @@ export default function Home() {
                                 </li>
                                 <li>
                                     <a href="https://github.com/ajobart/" target="_blank" className="btn-primary flex gap-1">
-                                            <Image path="/icons/github-logo-light.svg" className="size-4"></Image>
-                                            Github
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://gitlab.com/ajobart/" target="_blank" className="btn-primary flex gap-1">
-                                            <Image path="/icons/gitlab-logo-light.svg" className="size-4"></Image>
-                                            Gitlab
+                                        <Image path="/icons/github-logo-light.svg" className="size-4"></Image>
+                                        Github
                                     </a>
                                 </li>
                                 <li>
                                     <a href="mailto:jobart.pro@gmail.com" target="_blank" className="btn-primary flex gap-1">
-                                            <Image path="/icons/email-logo-light.svg" className="size-4"></Image>
-                                            Email
+                                        <Image path="/icons/email-logo-light.svg" className="size-4"></Image>
+                                        Get in touch
                                     </a>
                                 </li>
                             </ul>
                         </div>
                         <div className="h-full w-full flex items-center justify-center">
                             <div className="relative flex items-center justify-center h-full w-full">
-                                <div className="absolute z-[40] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-fit backdrop-blur-xl rounded-full overflow-hidden border-2 border-gray-500/20">
+                                {/* <div className="absolute z-[40] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-fit backdrop-blur-xl rounded-full overflow-hidden border-2 border-gray-500/20">
+                                    <Image path="/images/alexis-memoji.png" alt="hand" className="size-[260px]" />
+                                </div> */}
+                                {/* <div className="size-[180px] z-[2] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-blue-500">
+                                </div> */}
+                                <div className="absolute z-[40] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-fit">
                                     <Image path="/images/alexis-memoji.png" alt="hand" className="size-[260px]" />
                                 </div>
-                                <div className="size-[180px] z-[2] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-blue-500">
+                                <div className="herobanner-glow z-[2] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="h-fit w-full mb-4">
+                    {/* <div className="h-fit w-full mb-4">
                         <div className="flex flex-col items-start justify-start">
                             <h1 className="text-2xl font-bold">About</h1>
                             <p className="text-base font-normal">I grew up in the Normandy region of France, where I pursued business studies and obtained a Bachelor's degree. During my studies, I started coding websites in my spare time, which quickly became a passion. After graduating, I decided to follow what I truly love and enrolled in the "Le Wagon" bootcamp in Paris, where I perfected my development skills.</p>
@@ -93,6 +92,36 @@ export default function Home() {
                         </div>
                         <div>
 
+                        </div>
+                    </div> */}
+                    {/* SECTION ABOUT */}
+                    <div className="h-fit w-full flex flex-col items-center justify-center mb-20 mt-20 gap-8">
+                        <h1 className="text-4xl font-bold">ABOUT</h1>
+                        <div className="flex gap-4">
+                            {/* CARD */}
+                            <div className="w-full">
+                                <div className="h-fit w-full rounded-lg p-4 box-border backdrop-blur-md dark:bg-white/10 bg-[#D5EFFF] border border-gray-50/20">
+                                    <ul className="flex flex-row items-center justify-between">
+                                        <li className="flex flex-col items-center justify-center gap-1">
+                                            <p className="font-bold dark:text-portfolio-blue-dark text-portfolio-blue-light text-xl">3+</p>
+                                            <p className="font-medium text-white text-lg">YEARS EXP+</p>
+                                        </li>
+                                        <li className="flex flex-col items-center justify-center gap-1">
+                                            <p className="font-bold dark:text-portfolio-blue-dark text-portfolio-blue-light text-xl">7+</p>
+                                            <p className="font-medium text-white text-lg">MISSIONS FINISHED</p>
+                                        </li>
+                                        <li className="flex flex-col items-center justify-center gap-1">
+                                            <p className="font-bold dark:text-portfolio-blue-dark text-portfolio-blue-light text-xl">3+</p>
+                                            <p className="font-medium text-white text-lg">YEARS EXP</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            {/* TEXT */}
+                            <div className="w-full flex flex-col items-start justify-start">
+                                <p className="text-base font-normal">I grew up in Normandy, where I pursued business studies and obtained a Bachelor's degree. During my studies, I started coding websites in my spare time, which quickly became a passion. After graduating, I decided to follow what I truly love and enrolled in the "Le Wagon" bootcamp in Paris, where I perfected my development skills.</p>
+                                <p className="text-base mt-2 font-normal">For the past three years, I've been based in Paris and have been making a living as a developer. I currently collaborate with the talented team at the FORKEYS agency, where I continue to grow and evolve in my craft.</p>
+                            </div>
                         </div>
                     </div>
                     <div className="h-[120px] w-full">
@@ -114,11 +143,9 @@ export default function Home() {
                             </ul>
                         </div>
                     </div>
-                    <div className="h-[520px] mb-[24px] rounded-xl w-full bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(0,0,0,0.00)_0%,_rgba(0,0,0,0.00)_55%,_rgba(136,42,255,0.3)_66%,_rgba(162,88,255,_0.7)_80%,_#E0C8FF_100%)]">
-                        <h1 className="bg-gradient-to-r from-[rgba(255,_255,_255,_0.40)] via-[#fff] to-[rgba(255,_255,_255,_0.40)] bg-clip-text text-center text-xl font-medium !leading-normal text-transparent xs:text-2xl sm:text-4xl md:text-5xl lg:text-[56px]">
-                            Hello World Little Test
-                        </h1>
-                    </div>
+                    {/* Test radial gradient */}
+                    {/* <div className="h-[520px] mb-[24px] rounded-xl w-full bg-[radial-gradient(100%_100%_at_50%_0%,_rgba(0,0,0,0.00)_0%,_rgba(0,0,0,0.00)_55%,_rgba(136,42,255,0.3)_66%,_rgba(162,88,255,_0.7)_80%,_#E0C8FF_100%)]">
+                    </div> */}
                 </div>
             </div>
             <Footer />
